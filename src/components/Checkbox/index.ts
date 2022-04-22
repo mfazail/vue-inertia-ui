ab68c354d40e3f1f82c78f5c6b2c0ffacc096153
Fazail Alam <fazailalam898@gmail.com>
2022-04-22 11:34:09 +0530

    few more components

---
+++ src/components/Checkbox/index.ts
@@ -0,0 +1,2 @@
+import Button from "./Checkbox.vue";
+export default Button 
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,7 @@
-// import Button from './Button'
-// import Card from './Card'
-// import Input from './Input'
-// import Select from './Select'
-
 export { default as ViButton } from './Button/index'
 export { default as ViCard } from './Card/index'
 export { default as ViInput } from './Input/index'
 export { default as ViSelect } from './Select/index'
+export { default as ViCheckbox } from './Checkbox/index'
+export { default as ViRadio } from './Radio/index'
+export { default as ViToggle } from './Toggle/index'
